69edb781f4d8b6567f52f032261d3f65e30ac93a
tomadriaens <tomadriaens@hotmail.com>
2026-09-17 11:41:12 +0200

    [LES] Design Forms afgewerkt

---
+++ TomA_Lopen/TomA_Lopen/GUI/FrmBewerken.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TomA_Lopen
+{
+    partial class FrmBewerken
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKiesDeelnemer = new System.Windows.Forms.Label();
+            this.cmbKiesDeelnemer = new System.Windows.Forms.ComboBox();
+            this.btnAnnuleren = new System.Windows.Forms.Button();
+            this.btnAanpassen = new System.Windows.Forms.Button();
+            this.grbBetaald = new System.Windows.Forms.GroupBox();
+            this.rbNietBetaald = new System.Windows.Forms.RadioButton();
+            this.rbBetaald = new System.Windows.Forms.RadioButton();
+            this.cmbAfstand = new System.Windows.Forms.ComboBox();
+            this.txtNaam = new System.Windows.Forms.TextBox();
+            this.lblAfstand = new System.Windows.Forms.Label();
+            this.lblNaam = new System.Windows.Forms.Label();
+            this.btnVerwijderen = new System.Windows.Forms.Button();
+            this.grbBetaald.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblKiesDeelnemer
+            // 
+            this.lblKiesDeelnemer.AutoSize = true;
+            this.lblKiesDeelnemer.Location = new System.Drawing.Point(23, 23);
+            this.lblKiesDeelnemer.Name = "lblKiesDeelnemer";
+            this.lblKiesDeelnemer.Size = new System.Drawing.Size(155, 24);
+            this.lblKiesDeelnemer.TabIndex = 0;
+            this.lblKiesDeelnemer.Text = "Kies Deelnemer: ";
+            // 
+            // cmbKiesDeelnemer
+            // 
+            this.cmbKiesDeelnemer.FormattingEnabled = true;
+            this.cmbKiesDeelnemer.Location = new System.Drawing.Point(184, 20);
+            this.cmbKiesDeelnemer.Name = "cmbKiesDeelnemer";
+            this.cmbKiesDeelnemer.Size = new System.Drawing.Size(197, 32);
+            this.cmbKiesDeelnemer.TabIndex = 1;
+            // 
+            // btnAnnuleren
+            // 
+            this.btnAnnuleren.Location = new System.Drawing.Point(122, 348);
+            this.btnAnnuleren.Name = "btnAnnuleren";
+            this.btnAnnuleren.Size = new System.Drawing.Size(160, 51);
+            this.btnAnnuleren.TabIndex = 13;
+            this.btnAnnuleren.Text = "Annuleren";
+            this.btnAnnuleren.UseVisualStyleBackColor = true;
+            this.btnAnnuleren.Click += new System.EventHandler(this.btnAnnuleren_Click);
+            // 
+            // btnAanpassen
+            // 
+            this.btnAanpassen.Location = new System.Drawing.Point(27, 291);
+            this.btnAanpassen.Name = "btnAanpassen";
+            this.btnAanpassen.Size = new System.Drawing.Size(160, 51);
+            this.btnAanpassen.TabIndex = 12;
+            this.btnAanpassen.Text = "Aanpassen";
+            this.btnAanpassen.UseVisualStyleBackColor = true;
+            // 
+            // grbBetaald
+            // 
+            this.grbBetaald.Controls.Add(this.rbNietBetaald);
+            this.grbBetaald.Controls.Add(this.rbBetaald);
+            this.grbBetaald.Location = new System.Drawing.Point(27, 173);
+            this.grbBetaald.Name = "grbBetaald";
+            this.grbBetaald.Size = new System.Drawing.Size(355, 100);
+            this.grbBetaald.TabIndex = 11;
+            this.grbBetaald.TabStop = false;
+            this.grbBetaald.Text = "Heeft de persoon betaald?";
+            // 
+            // rbNietBetaald
+            // 
+            this.rbNietBetaald.AutoSize = true;
+            this.rbNietBetaald.Location = new System.Drawing.Point(27, 62);
+            this.rbNietBetaald.Name = "rbNietBetaald";
+            this.rbNietBetaald.Size = new System.Drawing.Size(64, 28);
+            this.rbNietBetaald.TabIndex = 1;
+            this.rbNietBetaald.TabStop = true;
+            this.rbNietBetaald.Text = "Nee";
+            this.rbNietBetaald.UseVisualStyleBackColor = true;
+            // 
+            // rbBetaald
+            // 
+            this.rbBetaald.AutoSize = true;
+            this.rbBetaald.Location = new System.Drawing.Point(27, 28);
+            this.rbBetaald.Name = "rbBetaald";
+            this.rbBetaald.Size = new System.Drawing.Size(47, 28);
+            this.rbBetaald.TabIndex = 0;
+            this.rbBetaald.TabStop = true;
+            this.rbBetaald.Text = "Ja";
+            this.rbBetaald.UseVisualStyleBackColor = true;
+            // 
+            // cmbAfstand
+            // 
+            this.cmbAfstand.FormattingEnabled = true;
+            this.cmbAfstand.Items.AddRange(new object[] {
+            "  5 km",
+            "10 km",
+            "20 km"});
+            this.cmbAfstand.Location = new System.Drawing.Point(185, 116);
+            this.cmbAfstand.Name = "cmbAfstand";
+            this.cmbAfstand.Size = new System.Drawing.Size(197, 32);
+            this.cmbAfstand.TabIndex = 10;
+            // 
+            // txtNaam
+            // 
+            this.txtNaam.Location = new System.Drawing.Point(185, 67);
+            this.txtNaam.Name = "txtNaam";
+            this.txtNaam.Size = new System.Drawing.Size(197, 29);
+            this.txtNaam.TabIndex = 9;
+            // 
+            // lblAfstand
+            // 
+            this.lblAfstand.AutoSize = true;
+            this.lblAfstand.Location = new System.Drawing.Point(23, 119);
+            this.lblAfstand.Name = "lblAfstand";
+            this.lblAfstand.Size = new System.Drawing.Size(120, 24);
+            this.lblAfstand.TabIndex = 8;
+            this.lblAfstand.Text = "Kies afstand: ";
+            // 
+            // lblNaam
+            // 
+            this.lblNaam.AutoSize = true;
+            this.lblNaam.Location = new System.Drawing.Point(23, 70);
+            this.lblNaam.Name = "lblNaam";
+            this.lblNaam.Size = new System.Drawing.Size(70, 24);
+            this.lblNaam.TabIndex = 7;
+            this.lblNaam.Text = "Naam: ";
+            // 
+            // btnVerwijderen
+            // 
+            this.btnVerwijderen.Location = new System.Drawing.Point(222, 291);
+            this.btnVerwijderen.Name = "btnVerwijderen";
+            this.btnVerwijderen.Size = new System.Drawing.Size(160, 51);
+            this.btnVerwijderen.TabIndex = 14;
+            this.btnVerwijderen.Text = "Verwijderen";
+            this.btnVerwijderen.UseVisualStyleBackColor = true;
+            // 
+            // FrmBewerken
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 436);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnVerwijderen);
+            this.Controls.Add(this.btnAnnuleren);
+            this.Controls.Add(this.btnAanpassen);
+            this.Controls.Add(this.grbBetaald);
+            this.Controls.Add(this.cmbAfstand);
+            this.Controls.Add(this.txtNaam);
+            this.Controls.Add(this.lblAfstand);
+            this.Controls.Add(this.lblNaam);
+            this.Controls.Add(this.cmbKiesDeelnemer);
+            this.Controls.Add(this.lblKiesDeelnemer);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "FrmBewerken";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bewerken van deelnemer: ";
+            this.grbBetaald.ResumeLayout(false);
+            this.grbBetaald.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKiesDeelnemer;
+        private System.Windows.Forms.ComboBox cmbKiesDeelnemer;
+        private System.Windows.Forms.Button btnAnnuleren;
+        private System.Windows.Forms.Button btnAanpassen;
+        private System.Windows.Forms.GroupBox grbBetaald;
+        private System.Windows.Forms.RadioButton rbNietBetaald;
+        private System.Windows.Forms.RadioButton rbBetaald;
+        private System.Windows.Forms.ComboBox cmbAfstand;
+        private System.Windows.Forms.TextBox txtNaam;
+        private System.Windows.Forms.Label lblAfstand;
+        private System.Windows.Forms.Label lblNaam;
+        private System.Windows.Forms.Button btnVerwijderen;
+    }
+}
---
+++ TomA_Lopen/TomA_Lopen/GUI/FrmOpzoeken.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TomA_Lopen
+{
+    partial class FrmOpzoeken
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1Deelnemer = new System.Windows.Forms.Button();
+            this.btnAnnuleren = new System.Windows.Forms.Button();
+            this.btnAlleDeelnemers = new System.Windows.Forms.Button();
+            this.cmbKiesDeelnemer = new System.Windows.Forms.ComboBox();
+            this.lblKiesDeelnemer = new System.Windows.Forms.Label();
+            this.lblToon = new System.Windows.Forms.Label();
+            this.txtToon = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1Deelnemer
+            // 
+            this.btn1Deelnemer.Location = new System.Drawing.Point(178, 12);
+            this.btn1Deelnemer.Name = "btn1Deelnemer";
+            this.btn1Deelnemer.Size = new System.Drawing.Size(160, 51);
+            this.btn1Deelnemer.TabIndex = 17;
+            this.btn1Deelnemer.Text = "1 deelnemer";
+            this.btn1Deelnemer.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuleren
+            // 
+            this.btnAnnuleren.Location = new System.Drawing.Point(344, 12);
+            this.btnAnnuleren.Name = "btnAnnuleren";
+            this.btnAnnuleren.Size = new System.Drawing.Size(160, 51);
+            this.btnAnnuleren.TabIndex = 16;
+            this.btnAnnuleren.Text = "Annuleren";
+            this.btnAnnuleren.UseVisualStyleBackColor = true;
+            this.btnAnnuleren.Click += new System.EventHandler(this.btnAnnuleren_Click);
+            // 
+            // btnAlleDeelnemers
+            // 
+            this.btnAlleDeelnemers.Location = new System.Drawing.Point(12, 12);
+            this.btnAlleDeelnemers.Name = "btnAlleDeelnemers";
+            this.btnAlleDeelnemers.Size = new System.Drawing.Size(160, 51);
+            this.btnAlleDeelnemers.TabIndex = 15;
+            this.btnAlleDeelnemers.Text = "Alle deelnemers";
+            this.btnAlleDeelnemers.UseVisualStyleBackColor = true;
+            // 
+            // cmbKiesDeelnemer
+            // 
+            this.cmbKiesDeelnemer.FormattingEnabled = true;
+            this.cmbKiesDeelnemer.Location = new System.Drawing.Point(178, 95);
+            this.cmbKiesDeelnemer.Name = "cmbKiesDeelnemer";
+            this.cmbKiesDeelnemer.Size = new System.Drawing.Size(326, 32);
+            this.cmbKiesDeelnemer.TabIndex = 19;
+            // 
+            // lblKiesDeelnemer
+            // 
+            this.lblKiesDeelnemer.AutoSize = true;
+            this.lblKiesDeelnemer.Location = new System.Drawing.Point(17, 98);
+            this.lblKiesDeelnemer.Name = "lblKiesDeelnemer";
+            this.lblKiesDeelnemer.Size = new System.Drawing.Size(155, 24);
+            this.lblKiesDeelnemer.TabIndex = 18;
+            this.lblKiesDeelnemer.Text = "Kies Deelnemer: ";
+            // 
+            // lblToon
+            // 
+            this.lblToon.AutoSize = true;
+            this.lblToon.Location = new System.Drawing.Point(17, 160);
+            this.lblToon.Name = "lblToon";
+            this.lblToon.Size = new System.Drawing.Size(125, 24);
+            this.lblToon.TabIndex = 20;
+            this.lblToon.Text = "De resultaten:";
+            // 
+            // txtToon
+            // 
+            this.txtToon.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtToon.Location = new System.Drawing.Point(12, 196);
+            this.txtToon.Multiline = true;
+            this.txtToon.Name = "txtToon";
+            this.txtToon.ReadOnly = true;
+            this.txtToon.Size = new System.Drawing.Size(492, 245);
+            this.txtToon.TabIndex = 21;
+            // 
+            // FrmOpzoeken
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 458);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtToon);
+            this.Controls.Add(this.lblToon);
+            this.Controls.Add(this.cmbKiesDeelnemer);
+            this.Controls.Add(this.lblKiesDeelnemer);
+            this.Controls.Add(this.btn1Deelnemer);
+            this.Controls.Add(this.btnAnnuleren);
+            this.Controls.Add(this.btnAlleDeelnemers);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "FrmOpzoeken";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Opzoeken van deelnemers:";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1Deelnemer;
+        private System.Windows.Forms.Button btnAnnuleren;
+        private System.Windows.Forms.Button btnAlleDeelnemers;
+        private System.Windows.Forms.ComboBox cmbKiesDeelnemer;
+        private System.Windows.Forms.Label lblKiesDeelnemer;
+        private System.Windows.Forms.Label lblToon;
+        private System.Windows.Forms.TextBox txtToon;
+    }
+}
